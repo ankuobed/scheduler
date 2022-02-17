4a82d9e715127ec20800be0c4f3369a36cae07ad
oaelikem <obed@myshabo.com>
2022-02-17 22:32:39 +0000

    fix: env error on production

--- main.go
+++ main.go
@@ -11,9 +11,12 @@ import (
 
 func main() {
 	// load .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+
+	if os.Getenv("APP_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
